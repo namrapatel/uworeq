226c6b2f731870f6e4d76f14d529e8cc3eca4ced
Namra Patel <namrajpatel@gmail.com>
2022-08-20 18:16:13 -0400

    progress: data for each course

--- course-scraper/scrape.ts
+++ course-scraper/scrape.ts
@@ -3,10 +3,13 @@ import cheerio from "cheerio";
 
 interface Course {
     name: string;
-    category: string;
+    courseNumber: number;
+    courseLetter: string[] | string;
+    breadthCategory: string;
     url: string;
-    credits: number;
-    description: string;
+    courseWeight: number;
+    formalSubjectName: string;
+
 }
 
 interface Subject {
@@ -50,6 +53,7 @@ async function getCourses() {
         );
     })
 
+    // Get the course list for each subject in the table
     for (var i = 0; i < subjects.length; i++) {
         const response1 = await gotScraping(subjects[i].url);
         const html1 = response1.body;
@@ -64,13 +68,73 @@ async function getCourses() {
             // add fullUrl to courses in subjects array
             subjects[i].courses[index] = ({
                 name: "",
-                category: "",
+                courseNumber: null,
+                courseLetter: "",
+                breadthCategory: "",
                 url: fullUrl,
-                credits: 0,
-                description: ""
+                courseWeight: 0,
+                formalSubjectName: ""
             });
         });
     }
+
+    // Get course name, course category, course credits, and course description for each course in the course array
+
+    // for (var i = 0; i < subjects.length; i++) {
+    //     for (var j = 0; j < subjects[i].courses.length; j++) {
+            const response2 = await gotScraping("https://westerncalendar.uwo.ca/Courses.cfm?CourseAcadCalendarID=MAIN_022436_1&SelectedCalendar=Live&ArchiveID=");
+            const html2 = response2.body;
+            const $2 = cheerio.load(html2);
+            
+            // Find div with id CourseInformationDiv and get its last child's text
+            const courseWeight = $2('div#CourseInformationDiv').children().last().text();
+            
+            // Match text "Course Weight:" and get the number after the colon
+            const courseWeightMatch = courseWeight.match(/Course Weight: (\d+\.\d+)/);
+            // console.log(courseWeightMatch[0]);
+            if (courseWeightMatch) {
+                console.log(courseWeightMatch[1]);
+                subjects[0].courses[0].courseWeight = parseInt(courseWeightMatch[1]);
+            }
+            
+            // Match text "Breadth:" and get the text after the colon
+            const breadthMatch = courseWeight.match(/Breadth: (.*)/);
+            if (breadthMatch) {
+                console.log("breadth1: "+breadthMatch[1]);
+                console.log("breadth0: "+breadthMatch[0])
+                subjects[0].courses[0].breadthCategory = breadthMatch[1];
+            }
+
+            // Match text "Subject Code:" and get the text after the colon
+            const subjectCodeMatch = courseWeight.match(/Subject Code: (\w+)/);
+            if (subjectCodeMatch) {
+                subjects[0].courses[0].formalSubjectName = subjectCodeMatch[1];
+            }
+
+            // Find div with id CourseInformationDiv and get the h3's text from within its first child
+            subjects[0].courses[0].name = $2('div#CourseInformationDiv > div.col-md-12 > h3').text();
+
+            const preCourseNumber = $2('div#CourseInformationDiv > div.col-md-12 > h2').text();
+            // Split the preCourseNumber string into an array of strings
+            const temp1 = preCourseNumber.split(" ");
+            // Get the string that starts with a number from courseNumber array
+            const courseNumberString = temp1.find(string => /^\d+/.test(string));
+            // Get the first four chars of the courseNumberString
+            subjects[0].courses[0].courseNumber = parseInt(courseNumberString.substring(0, 4));
+            // Get everything except the first four chars of the courseNumberString
+            const courseLetter = courseNumberString.substring(4);
+            // Match all letters in courseLeter and put each in an array
+            const letters = courseLetter.match(/[a-zA-Z]+/g);
+            if (letters) {
+                subjects[0].courses[0].courseLetter = letters;
+            }
+    //     }
+    // }
+    console.log(subjects[0].courses[0].name);
+    console.log(subjects[0].courses[0].courseLetter);
+    console.log(subjects[0].courses[0].courseNumber);
+    console.log(subjects[0].courses[0].courseWeight);
+    console.log(subjects[0].courses[0].breadthCategory);
 }
 
 console.log(getCourses())
